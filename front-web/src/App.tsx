d1ad83fdc3bbe30e5886990c77541bed39805c57
Ronellyson <ronellysonjulio@gmail.com>
2021-01-09 19:57:19 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Home from './Home/index'
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar/>
-    </div>
+      <Home/>
+    </>
   );
 }
 
